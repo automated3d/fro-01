92837fb6bb949f675d21e0186adb1232959061b7
automated3d <111648024+automated3d@users.noreply.github.com>
2022-08-31 18:56:39 -0600

    HTML formulario de registro
    Se agrego la estructura del formulario de registro

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//let num1 = 7;
+//let num2 = 9;
+//let result1 = num1 + num2;
+//let result2 = num1 * num2;
+
+//console.log('Result1 is equal to', result1);
+//console.log('Result2 is equal to', result2);
+
+let h2Nombre = document.getElementById('nombre-personaje');
+
+h2Nombre.textContent = h2Nombre.textContent.toUpperCase();
+
+//Imprimir el text que tiene el elemento ReadableStreamDefaultController
+
+console.log("Contenido: ", h2Nombre.textContent);
+console.log("Nombre de la clase ", h2Nombre.className);
+console.log("id ", h2Nombre);
